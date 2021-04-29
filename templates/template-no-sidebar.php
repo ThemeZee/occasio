4be63372206ff6cf653cf755ff9fc79e62774e0d
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-04-29 13:06:30 +0200

    added Related Posts to No Sidebar template

--- templates/template-no-sidebar.php
+++ templates/template-no-sidebar.php
@@ -17,7 +17,7 @@ while ( have_posts() ) :
 
 	occasio_post_navigation();
 
-	do_action( 'occasio_related_posts' );
+	occasio_related_posts();
 
 	// If comments are open or we have at least one comment, load up the comment template.
 	if ( comments_open() || get_comments_number() ) :
