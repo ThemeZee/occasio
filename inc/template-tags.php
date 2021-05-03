7a686b3f022f72e45ab9d1d3c6067706d8b634c7
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-03 18:08:45 +0200

    fixed typo in pagination function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -358,7 +358,7 @@ if ( ! function_exists( 'occasio_pagination' ) ) :
 
 		the_posts_pagination( array(
 			'mid_size'  => 2,
-			'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'occasio' ) . '</span>',
+			'prev_text' => '&laquo;<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'occasio' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'occasio' ) . '</span>&raquo;',
 		) );
 
